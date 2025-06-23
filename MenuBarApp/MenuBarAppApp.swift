83ea8e59eb839c27c1f9c7f0edd836137d81faa3
zoli0829 <zoli0829@gmail.com>
2025-06-23 17:26:54 +0100

    Initial commit

---
+++ MenuBarApp/MenuBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarAppApp.swift
+//  MenuBarApp
+//
+//  Created by Zoltan Vegh on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
